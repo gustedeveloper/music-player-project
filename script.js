0e4588c8c780dce1a1b7fc427f7f18ff24bda8e1
gustedeveloper <219.guste@gmail.com>
2024-04-09 16:44:28 +0200

    Add initial variables for the music player

---
+++ script.js
@@ -0,0 +1,6 @@
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+const shuffleButton = document.getElementById('shuffle');
