2856affbd19d32f7eee71aacd3ef7d1fe806ca30
gustedeveloper <219.guste@gmail.com>
2024-04-29 18:36:27 +0200

    Add event listener to the audio element to automatically play the next song when the current song ends

--- script.js
+++ script.js
@@ -258,9 +258,27 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+
 nextButton.addEventListener("click", playNextSong);
+
 previousButton.addEventListener("click", playPreviousSong);
+
 shuffleButton.addEventListener("click", shuffle);
+
+audio.addEventListener("ended", () => {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+        playNextSong();
+        userData.currentSong = null;
+        userData.songCurrentTime = 0;
+        pauseSong();
+        setPlayerDisplay();
+        highlightCurrentSong();
+        setPlayButtonAccessibleText();
+    }
+})
 
 const sortSongs = () =>  {
     userData?.songs.sort((a, b) => {
